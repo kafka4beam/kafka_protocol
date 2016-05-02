c82ce833f34975f28ffeb2bd5b0a4c478b8899f7
Dan Griffin <dgriff1@gmail.com>
2016-05-01 19:17:24 -0600

    code cleanup

--- src/kpro.erl
+++ src/kpro.erl
@@ -340,13 +340,13 @@ decode_message_stream(Bin, Acc) ->
     catch error : {badmatch, _} ->
       {?incomplete_message, <<>>}
     end,
-  if
-    Msg#kpro_Message.attributes == ?KPRO_COMPRESS_GZIP ->
-      Messages  = decode_message_stream(zlib:gunzip(Msg#kpro_Message.value), Acc),
-      decode_message_stream(Rest, Messages);
-    true ->
-      decode_message_stream(Rest, [Msg | Acc])
-  end.
+  Messages = case Msg#kpro_Message.attributes of
+    ?KPRO_COMPRESS_GZIP ->
+      decode_message_stream(zlib:gunzip(Msg#kpro_Message.value), Acc);
+    _Else ->
+      [Msg | Acc]
+  end,
+  decode_message_stream(Rest, Messages).
 
 decode_fields(RecordName, Fields, Bin) ->
   {FieldValues, BinRest} = do_decode_fields(RecordName, Fields, Bin, _Acc = []),
