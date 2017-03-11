1b745b12f6782346f374c382e999b392c78607dd
Zaiming Shi <zmstone@gmail.com>
2017-03-11 12:59:49 -0500

    update copyright

--- src/kpro.erl
+++ src/kpro.erl
@@ -1,4 +1,4 @@
-%%%   Copyright (c) 2014-2016, Klarna AB
+%%%   Copyright (c) 2014-2017, Klarna AB
 %%%
 %%%   Licensed under the Apache License, Version 2.0 (the "License");
 %%%   you may not use this file except in compliance with the License.
