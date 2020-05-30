f309d5945c4315382be2b7c6cf7bc3b385d2aaa0
Zaiming Shi <zmstone@gmail.com>
2020-05-30 15:24:43 +0200

    Fix consumer group tests

--- test/kpro_group_tests.erl
+++ test/kpro_group_tests.erl
@@ -38,30 +38,38 @@
 %% 3. sync_group, leader assign partitions, members receive assignments
 %% 4. heartbeat-cycle, to tell broker that it is still alive
 %% 5. leave_group
-full_flow_test() ->
+full_flow_test_() ->
+  [{atom_to_list(KafkaVsn),
+    fun() -> test_full_flow(KafkaVsn) end }
+   || KafkaVsn <- kafka_vsns()].
+
+test_full_flow(KafkaVsn) ->
   GroupId = make_group_id(full_flow_test),
-  StaticMemberID = <<"member-1">>,
+  StaticMemberID = <<>>,
   % find_coordinator (group)
   {ok, Connection} = connect_coordinator(GroupId),
   % join_group
   #{ member_id := MemberId
    , generation_id := Generation
-   } = join_group(Connection, GroupId, StaticMemberID),
-  ok = sync_group(Connection, GroupId, MemberId, Generation, StaticMemberID),
+   } = join_group(Connection, GroupId, StaticMemberID, KafkaVsn),
+  ok = sync_group(Connection, GroupId, MemberId, Generation,
+                  StaticMemberID, KafkaVsn),
   % send hartbeats, there should be a generation_id in heartbeat requests,
   % generation bumps whenever there is a group re-balance, however since
   % we are testing with only one group member, we do not expect any group
   % rebalancing, hence generation_id should not change (alwyas send the same)
-  F = fun() -> heartbeat(Connection, GroupId, MemberId, Generation) end,
+  F = fun() ->
+          heartbeat(Connection, GroupId, MemberId, Generation, KafkaVsn)
+      end,
   {HeartbeatPid, Mref} = erlang:spawn_monitor(F),
-  ok = describe_groups(Connection, GroupId),
-  ok = list_groups(Connection),
+  ok = describe_groups(Connection, GroupId, KafkaVsn),
+  ok = list_groups(Connection, KafkaVsn),
   HeartbeatPid ! stop,
   receive
     {'DOWN', Mref, process, HeartbeatPid, Reason} ->
       ?assertEqual(normal, Reason)
   end,
-  ok = leave_group(Connection, GroupId, MemberId),
+  ok = leave_group(Connection, GroupId, MemberId, KafkaVsn),
   ok = kpro:close_connection(Connection),
   ok.
 
@@ -74,38 +82,55 @@ connect_coordinator(GroupId) ->
   kpro:connect_coordinator(Cluster, ConnCfg, Args).
 
 %% NOTE: MemberId is only relevant for join_group request version 4 or later.
-join_group(Connection, GroupId, StaticMemberId) ->
+join_group(Connection, GroupId, StaticMemberId, KafkaVsn) ->
   Meta = #{ version => 0
           , topics => [?TOPIC]
           , user_data => <<"magic-1">>
           },
-  Fields = #{ group_id => GroupId
-            , session_timeout_ms => timer:seconds(10)
-            , rebalance_timeout_ms => timer:seconds(10)
-            , member_id => <<>>
-            , protocol_type => <<"consumer">>
-            , group_instance_id  => StaticMemberId
-            , protocols =>
-                [ #{ name => <<"test-v1">>
-                   , metadata => Meta
-                   }
-                ]
-            },
-  Rsp = rand_vsn_request_sync(Connection, join_group, Fields),
+  Send =
+    fun(MemberId) ->
+      Fields = #{ group_id => GroupId
+                , session_timeout_ms => timer:seconds(10)
+                , rebalance_timeout_ms => timer:seconds(10)
+                , member_id => MemberId
+                , protocol_type => <<"consumer">>
+                , group_instance_id  => StaticMemberId
+                , protocols =>
+                    [ #{ name => <<"test-v1">>
+                      , metadata => Meta
+                      }
+                    ]
+                },
+      request_sync(Connection, join_group, Fields, KafkaVsn)
+    end,
+  #{ error_code := ErrorCode
+   , member_id := MyMemberId
+   } = Rsp0 = Send(<<>>),
+  Rsp =
+    case ErrorCode of
+      no_error ->
+        Rsp0;
+      member_id_required ->
+        %% https://cwiki.apache.org/confluence/display/KAFKA/KIP-394
+        %% Since kafka 2.2, members will maybe receive old member ID
+        %% with a member_id_required error code
+        Send(MyMemberId)
+    end,
   #{ error_code := no_error
    , protocol_name := <<"test-v1">>
    , leader := LeaderId
-   , member_id := MemberId
-   , members := [#{ member_id := MemberId1
+   , member_id := MyMemberId
+   , members := [#{ member_id := MemberId
                   , metadata := Meta
                   }]
    } = Rsp,
   %% only member in grou, leader must be self
-  ?assertEqual(LeaderId, MemberId),
-  ?assertEqual(MemberId, MemberId1),
+  ?assertEqual(LeaderId, MyMemberId),
+  ?assertEqual(MyMemberId, MemberId),
   Rsp.
 
-sync_group(Connection, GroupId, MemberId, Generation, StaticMemberId) ->
+sync_group(Connection, GroupId, MemberId, Generation,
+           StaticMemberId, KafkaVsn) ->
   MemberAssignment =
     #{ version => 0
      , topic_partitions => [#{ topic => ?TOPIC
@@ -124,45 +149,44 @@ sync_group(Connection, GroupId, MemberId, Generation, StaticMemberId) ->
             , assignments => Assignment
             , group_instance_id => StaticMemberId
             },
-  Rsp = rand_vsn_request_sync(Connection, sync_group, Fields),
+  Rsp = request_sync(Connection, sync_group, Fields, KafkaVsn),
   #{ error_code := no_error
    , assignment := MemberAssignment
    } = Rsp,
   ok.
 
-describe_groups(Connection, GroupId) ->
+describe_groups(Connection, GroupId, KafkaVsn) ->
   Groups = [GroupId, <<"unknown-group">>],
   Body = #{groups => Groups, include_authorized_operations => true},
-  Rsp = rand_vsn_request_sync(Connection, describe_groups, Body),
+  Rsp = request_sync(Connection, describe_groups, Body, KafkaVsn),
   #{groups := RspGroups} = Rsp,
   lists:foreach(
     fun(#{error_code := ErrorCode}) ->
         ?assertEqual(no_error, ErrorCode)
     end, RspGroups).
 
-list_groups(Connection) ->
-  Rsp = rand_vsn_request_sync(Connection, list_groups, #{}),
+list_groups(Connection, KafkaVsn) ->
+  Rsp = request_sync(Connection, list_groups, #{}, KafkaVsn),
   ?assertMatch(#{error_code := no_error}, Rsp),
   ok.
 
-leave_group(Connection, GroupId, MemberId) ->
+leave_group(Connection, GroupId, MemberId, KafkaVsn) ->
   Fields = #{ group_id => GroupId
             , member_id => MemberId
             },
-  Rsp = rand_vsn_request_sync(Connection, leave_group, Fields),
+  Rsp = request_sync(Connection, leave_group, Fields, KafkaVsn),
   ?assertMatch(#{error_code := no_error}, Rsp),
   ok.
 
-heartbeat(Connection, GroupId, MemberId, Generation) ->
-  {Min, Max} = get_api_vsn_range(Connection, heartbeat),
+heartbeat(Connection, GroupId, MemberId, Generation, KafkaVsn) ->
+  Vsn = maps:get(heartbeat, max_vsn(KafkaVsn)),
+  Req = kpro:make_request(heartbeat, Vsn,
+                          [ {group_id, GroupId}
+                          , {generation_id, Generation}
+                          , {member_id, MemberId}
+                          ]),
   SendFun =
     fun() ->
-        Vsn = rand(Min, Max),
-        Req = kpro:make_request(heartbeat, Vsn,
-                                [ {group_id, GroupId}
-                                , {generation_id, Generation}
-                                , {member_id, MemberId}
-                                ]),
         {ok, #kpro_rsp{msg = Rsp}} =
           kpro:request_sync(Connection, Req, ?TIMEOUT),
         ErrorCode = kpro:find(error_code, Rsp),
@@ -180,9 +204,8 @@ heartbeat_loop(SendFun) ->
       heartbeat_loop(SendFun)
   end.
 
-rand_vsn_request_sync(Connection, API, Body) ->
-  {Min, Max} = get_api_vsn_range(Connection, API),
-  Vsn = rand(Min, Max),
+request_sync(Connection, API, Body, KafkaVsn) ->
+  Vsn = maps:get(API, max_vsn(KafkaVsn)),
   Req = kpro:make_request(API, Vsn, Body),
   {ok, #kpro_rsp{msg = Rsp}} = kpro:request_sync(Connection, Req, ?TIMEOUT),
   Rsp.
@@ -200,22 +223,63 @@ rand() ->
   {_, _, I} = os:timestamp(),
   I.
 
-rand(Min, Max) ->
-  rand() rem (Max - Min + 1) + Min.
+kafka_vsns() ->
+  [v0_9, v0_10, v0_11, v1_0, v2_0, v2_1, v2_2
+   %%,v2_3, v2_4 % commented out, TODO: test group_instance_id
+  ].
 
-get_api_vsn_range(Connection, API) ->
-  {ok, KafkaSupportedRange} = kpro:get_api_vsn_range(Connection, API),
-  kpro_api_vsn:intersect(KafkaSupportedRange, range(API)).
-
-%% Max version for consumer group APIs are all before the introduction of
-%% group_instance_id (see KIP-345). we are not testing it so far.
-%% TODO: Add tests to support group_instance_id
-range(join_group) -> {0, 4};
-range(heartbeat) -> {0, 2};
-range(leave_group) -> {0, 2};
-range(sync_group) -> {0, 2};
-range(describe_groups) -> {0, 4};
-range(list_groups) -> {0, 100}.
+max_vsn(v0_9) -> max_vsn(v0_10);
+max_vsn(v0_10) ->
+  #{ join_group => 0
+   , heartbeat => 0
+   , leave_group => 0
+   , sync_group => 0
+   , describe_groups => 0
+   , list_groups => 0
+   };
+max_vsn(v0_11) -> max_vsn(v1_0);
+max_vsn(v1_0) -> max_vsn(v1_1);
+max_vsn(v1_1) ->
+  #{ join_group => 2
+   , heartbeat => 1
+   , leave_group => 1
+   , sync_group => 1
+   , describe_groups => 1
+   , list_groups => 1
+   };
+max_vsn(v2_0) -> max_vsn(v2_1);
+max_vsn(v2_1) ->
+  #{ join_group => 3
+   , heartbeat => 2
+   , leave_group => 2
+   , sync_group => 2
+   , describe_groups => 2
+   , list_groups => 2
+   };
+max_vsn(v2_2) ->
+  #{ join_group => 4
+   , heartbeat => 2
+   , leave_group => 2
+   , sync_group => 2
+   , describe_groups => 2
+   , list_groups => 2
+   };
+max_vsn(v2_3) ->
+  #{ join_group => 5
+   , heartbeat => 3
+   , leave_group => 2
+   , sync_group => 3
+   , describe_groups => 3
+   , list_groups => 2
+   };
+max_vsn(v2_4) ->
+  #{ join_group => 6
+   , heartbeat => 4
+   , leave_group => 4
+   , sync_group => 4
+   , describe_groups => 5
+   , list_groups => 3
+   }.
 
 %%%_* Emacs ====================================================================
 %%% Local Variables:
