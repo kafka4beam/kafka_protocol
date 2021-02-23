c83c26143e983aa4510ca0fc4afda34bbb7a0920
Roberto Aloi <robert.aloi@klarna.com>
2021-02-23 17:34:42 +0100

    Update Copyright years

--- include/kpro_private.hrl
+++ include/kpro_private.hrl
@@ -1,4 +1,4 @@
-%%%   Copyright (c) 2014-2020, Klarna AB
+%%%   Copyright (c) 2014-2021, Klarna Bank AB
 %%%
 %%%   Licensed under the Apache License, Version 2.0 (the "License");
 %%%   you may not use this file except in compliance with the License.
